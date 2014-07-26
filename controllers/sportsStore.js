9af98e077ce84c1802cadf63de202098a8a9e7d9
ybak <ybakswu@gmail.com>
2014-07-26 18:43:58 +0800

    add admin function

--- controllers/sportsStore.js
+++ controllers/sportsStore.js
@@ -1,10 +1,22 @@
 angular.module("sportsStore")
-    .constant("dataUrl","http://localhost:5500/products")
-    .controller("sportsStoreCtrl", function ($scope, $http, dataUrl) {
+    .constant("dataUrl", "http://localhost:5500/products")
+    .constant("orderUrl", "http://localhost:5500/orders")
+    .controller("sportsStoreCtrl", function ($scope, $http, $location, dataUrl, orderUrl, cart) {
         $scope.data = {};
-        $http.get(dataUrl).success(function(data){
+
+        $http.get(dataUrl).success(function (data) {
             $scope.data.products = data;
-        }).error(function(error){
+        }).error(function (error) {
             $scope.data.error = error;
         });
+
+        $scope.sendOrder = function (shippingDetails) {
+            var order = angular.copy(shippingDetails);
+            order.products = cart.getProducts();
+            $http.post(orderUrl, order).success(function (error) {
+                $scope.data.orderError = error;
+            }).finally(function () {
+                $location.path("/complete");
+            })
+        };
     });
